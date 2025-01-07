492fd322e847e840a753457b8e79a5f54bfb1ac7
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2025-01-07 16:03:51 +0800

    fix(ecommerce): fix image access to public

--- server/config/multerConfig.js
+++ server/config/multerConfig.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "public/images"));
+    cb(null, path.join(__dirname, "./public/images"));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ const shipmentRoutes = require("./routes/shipmentRoutes");
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
 const app = express();
 app.use(express.json());
@@ -27,8 +31,33 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const sessionStoreAdmin = new MySQLStore({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  clearExpired: true,
+  checkExpirationInterval: 900000,
+  expiration: 86400000,
+});
+
+app.use(
+  session({
+    name: "adminId",
+    secret: "N4EAGL",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStoreAdmin,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.use(customerRoutes);
 app.use(rewardRoutes);
@@ -47,6 +76,6 @@ app.use(authRoutes);
 app.use(adminRoutes);
 app.use(dashboardRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(8081, () => {
+  console.log(`Server running on http://localhost:8081`);
 });
